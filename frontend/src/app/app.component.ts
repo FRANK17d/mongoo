621ad4aca0f9d97ed86c1f1f619a483093d9c4b6
Frank Castro <frank.castro.g@tecsup.edu.pe>
2025-05-15 01:06:58 -0500

    agregue ese active

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { ErrorMessageComponent } from './components/error-message/error-message.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, ErrorMessageComponent],
+  imports: [RouterOutlet, CommonModule, ErrorMessageComponent, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
